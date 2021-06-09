0fe042f45ccb88c7d43dc587882b5dd41dd68dea
VincenzoMele <vincenzo.mele001@studenti.uniparthenope.it>
2021-06-09 12:29:25 +0200

    add quiz.py

--- venv/quiz.py
+++ venv/quiz.py
@@ -1,5 +1,5 @@
 quiz={
-    "nquiz":
+    "normal_quiz":
     {
     "Which function is the first function a C program calls?": ["printf()","scanf()","start()","main()"],
     "C is a:": ["Low level programming language","High level programming language","Markup Language","Not a language"],
@@ -10,9 +10,10 @@ quiz={
     "Which function prints something on the console?": ["scanf()","printf()","system()","main()"],
     "Which function reads an input from the console?": ["printf()","push()","start()","scanf()"],
     },
-    "quiz code":
+    "code_quiz":
     {
     'Select the output of the following code:\nint main() {\nint x=5;\nint y=3;\nx=x+y*x;\nprintf("%d",x);\nreturn 0;\n}': ["5","40","20","35"],
     "Complete the code to print the value of x:\nint main() {\nint x=5;\nx=x+4;\n_____________;\nreturn 0;\n}": ['printf("%d,&x")','printf("%d,x")','scanf("%d",&x)',"push(x)"]
-    }
+    },
+    "answers": ["main()","High level programming language","Word","/*comment*/",";","==","printf()","scanf()","20",'printf("%d,x")']
 }
